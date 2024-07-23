237a91638d6a33d30a719c9fef183cc7ef9444e0
lak-sh-man <lakshman1122000@gmail.com>
2024-07-23 12:20:33 +0530

    commited

--- user_site/user_home/views.py
+++ user_site/user_home/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request, Blueprint
 user_core = Blueprint('user_core', __name__, template_folder = '../templates')
 
 @user_core.route('/user_home')
-def home():
+def user_home():
     '''
     This is the home page view. Notice how it uses pagination to show a limited
     number of posts by limiting its query size and then calling paginate.
