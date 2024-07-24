9cb52379e509495bab56569016a4a5be601659b7
lak-sh-man <lakshman1122000@gmail.com>
2024-07-24 14:34:33 +0530

    commited

--- setup.py
+++ setup.py
@@ -3,18 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask import Flask
-from flask import render_template
 
 
 app = Flask(__name__, template_folder='templates')
-#########################################################################################
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 #########################################################################################
 app.config['SECRET_KEY'] = 'mysecret'
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -28,10 +19,3 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "user_login_bp.user_login"
 #########################################################################################
-from user_site.user_home.views import user_home_bp
-from user_site.user_login.views import user_login_bp
-from error_handlers import error_pages_bp
-
-app.register_blueprint(user_home_bp)
-app.register_blueprint(user_login_bp)
-app.register_blueprint(error_pages_bp)
