d6a839903d48bacaf483cddb59c0b739181b8231
lak-sh-man <lakshman1122000@gmail.com>
2024-07-24 12:03:03 +0530

    commited

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 from setup import app
-from flask import render_template
+# from flask import render_template
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
 
-from user_site.user_home.views import user_home_bp
-from user_site.user_login.views import user_login_bp
-from error_handlers import error_pages_bp
+# from user_site.user_home.views import user_home_bp
+# from user_site.user_login.views import user_login_bp
+# from error_handlers import error_pages_bp
 
-app.register_blueprint(user_home_bp)
-app.register_blueprint(user_login_bp)
-app.register_blueprint(error_pages_bp)
+# app.register_blueprint(user_home_bp)
+# app.register_blueprint(user_login_bp)
+# app.register_blueprint(error_pages_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
