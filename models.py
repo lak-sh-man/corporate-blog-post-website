779c54a4734b7b5d82e3d63ef3972358330725fe
lak-sh-man <lakshman1122000@gmail.com>
2024-07-27 23:12:19 +0530

    commited

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
 
-    __tablename__ = 'user'
+    __tablename__ = 'user_table'
 
     id = db.Column(db.Integer, primary_key = True)
     profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
@@ -33,13 +33,58 @@ class User(db.Model, UserMixin):
 
 class UserBlogPost(db.Model):
 
-    __tablename__ = 'blog_post'
+    __tablename__ = 'userblogpost_table'
 
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     title = db.Column(db.String(140), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_table.id'), nullable=False)
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id =user_id
+
+
+    def __repr__(self):
+        return f"Post Id: {self.id} --- Date: {self.date} --- Title: {self.title}"
+    
+
+
+class Admin(db.Model, UserMixin):
+
+    __tablename__ = 'admin_table'
+
+    id = db.Column(db.Integer, primary_key = True)
+    profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('AdminBlogPost', backref='admin_author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+
+
+
+class AdminBlogPost(db.Model):
+
+    __tablename__ = 'adminblogpost_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('admin_table.id'), nullable=False)
 
     def __init__(self, title, text, user_id):
         self.title = title
