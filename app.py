56f034e1d0c354f432cf946ebc4ffbcfe76de335
lak-sh-man <lakshman1122000@gmail.com>
2024-07-29 14:28:18 +0530

    commited

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
  
 from user_site.user_blog_post.views import user_blog_post_bp
 from user_site.user_authentication.views import user_authentication_bp
