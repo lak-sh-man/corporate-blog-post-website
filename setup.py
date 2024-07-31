e3f252956ec6d84dc66c6b958c7c28f1aab8a733
lak-sh-man <lakshman1122000@gmail.com>
2024-07-31 15:41:28 +0530

    commited

--- setup.py
+++ setup.py
@@ -19,3 +19,4 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "user_authentication_bp.user_login"
 #########################################################################################
+ 
